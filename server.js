909fbe0b9ae432b39e06a5e5fe8c9e616f9b4af1
Yash Janyani <yash.janyani2000@gmail.com>
2023-07-29 01:26:27 +0530

    old

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ app.use(cors());
 
 //routes
 //user routes
-app.use("/users", require("./routes/userRoute"));
+app.use("/api/v1/users", require("./routes/userRoute"));
 //transactions routes
-app.use("/transactions", require("./routes/transactionRoutes"));
+app.use("/api/v1/transactions", require("./routes/transactionRoutes"));
 
 //static files
 app.use(express.static(path.join(__dirname, "./client/build")));
